590671727f786e46d6f7888ba9740b836d06b625
kubo550 <43968748+kubo550@users.noreply.github.com>
2021-02-17 22:08:43 +0100

    Main sketch TS

---
+++ ts/sektch.ts
@@ -0,0 +1,33 @@
+import Circle from "./Circle.js"
+
+const canvas = document.querySelector("canvas")!;
+const ctx = canvas.getContext("2d")!;
+canvas.width = window.innerWidth - 2;
+canvas.height = window.innerHeight - 4;
+
+
+const resize = () => {
+    canvas.width = window.innerWidth - 2;
+    canvas.height = window.innerHeight - 4;
+}
+
+const circles: Circle[] = []
+
+window.addEventListener('resize', resize)
+
+window.addEventListener('mousemove', e => {
+    const circle = new Circle(ctx, e.clientX, e.clientY)
+    circles.push(circle)
+})
+
+
+const draw = () => {
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    circles.forEach(circle => circle.update())
+
+
+    requestAnimationFrame(draw)
+}
+draw()
